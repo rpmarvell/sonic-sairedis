d5cf2ae6c22bfa602a8b5829c95a1939aca24e56
rpmarvell <rperumal@marvell.com>
2024-12-11 06:41:41 -0800

    build fix

--- lib/ServerSai.h
+++ lib/ServerSai.h
@@ -307,12 +307,16 @@ namespace sairedis
 
             sai_service_method_table_t m_service_method_table;
 
-            std::shared_ptr<SaiInterface> m_sai;
 
             std::shared_ptr<std::thread> m_serverThread;
 
-            std::shared_ptr<SelectableChannel> m_selectableChannel;
 
             swss::SelectableEvent m_serverThreadThreadShouldEndEvent;
+
+        private:
+
+            std::shared_ptr<SelectableChannel> m_selectableChannel;
+
+            std::shared_ptr<SaiInterface> m_sai;
     };
 }
